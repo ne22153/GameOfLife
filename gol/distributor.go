b4dd5601e15ba741f1289a7a76b7191f74d94ca3
ec22583 <ec22583@bristol.ac.uk>
2023-10-27 20:35:07 +0100

    refactor the GoL algorithm code to be more readable

--- gol/distributor.go
+++ gol/distributor.go
@@ -33,6 +33,7 @@ func findAliveCells(p Params, world [][]uint8) []util.Cell {
 	return aliveCells
 }
 
+//Helper function to distributor to find the number of alive cells adjancent to the tile
 func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	coordinates := []util.Cell{}
 	for index, row := range world {
@@ -45,61 +46,105 @@ func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	return coordinates
 }
 
-func worker(p Params, inputWorld [][]uint8) [][]uint8 {
-	updatedWorld := make([][]uint8, p.ImageHeight)
+func updateUpdatedWorldTile(inputWorldTile, updatedWorldTile uint8, adjacentAliveCells int) uint8 {
+	// if the element is dead, then run through those checks
+	if inputWorldTile == DEAD {
+		if adjacentAliveCells > 0 {
+			//fmt.Println(num, ", world", index2, index)
+		}
+		if adjacentAliveCells == 3 {
+			updatedWorldTile = LIVE
+		} else {
+			updatedWorldTile = DEAD
+		}
 
-	for i := range updatedWorld {
-		updatedWorld[i] = make([]byte, p.ImageWidth)
+	} else {
+		if adjacentAliveCells < 2 || adjacentAliveCells > 3 {
+			updatedWorldTile = DEAD
+		} else {
+			updatedWorldTile = LIVE
+		}
+
 	}
 
-	for index, element := range inputWorld {
-		var prevElement, nextElement []byte
+	return updatedWorldTile
+}
 
-		prevElement = inputWorld[((index + len(updatedWorld) - 1) % len(updatedWorld))]
-		nextElement = inputWorld[(index+len(updatedWorld)+1)%len(updatedWorld)]
+func populateNeighboursList(inputWorld [][]uint8, prevRow, row, nextRow []uint8, edgeIndex, tileIndex int) []uint8 {
+	neighboursList := []uint8{
+		prevRow[tileIndex],
+		nextRow[tileIndex]}
 
-		var edge = p.ImageHeight - 1
+	//Boundary: check if the tile is to the very left of the screen, if so take left neighbours from the other side
+	if tileIndex > 0 {
+		neighboursList = append(
+			neighboursList,
+			row[(tileIndex-1)%len(inputWorld)],
+			prevRow[(tileIndex-1)%len(row)],
+			nextRow[(tileIndex-1)%len(row)])
+	} else {
+		neighboursList = append(
+			neighboursList,
+			row[edgeIndex],
+			prevRow[edgeIndex],
+			nextRow[edgeIndex])
+	}
 
-		for index2, element2 := range element {
-			// makes a list of the neighbours of the element2
-			values := []byte{prevElement[index2], nextElement[index2]}
-			if index2 > 0 {
-				values = append(values, element[(index2-1)%len(inputWorld)], prevElement[(index2-1)%len(element)], nextElement[(index2-1)%len(element)])
-			} else {
-				values = append(values, element[edge], prevElement[edge], nextElement[edge])
-			}
-			if index2 < edge {
-				values = append(values, element[(index2+1)%len(element)], prevElement[(index2+1)%len(element)], nextElement[(index2+1)%len(element)])
-			} else {
-				values = append(values, element[0], prevElement[0], nextElement[0])
-			}
-			num := 0
-			for _, value := range values {
+	//Boundary: check if the tile is to the very right of the screen, if so take right neighbours from the other side
+	if tileIndex < edgeIndex {
+		neighboursList = append(
+			neighboursList,
+			row[(tileIndex+1)%len(row)],
+			prevRow[(tileIndex+1)%len(row)],
+			nextRow[(tileIndex+1)%len(row)])
+	} else {
+		neighboursList = append(
+			neighboursList,
+			row[0],
+			prevRow[0],
+			nextRow[0])
+	}
+
+	return neighboursList
+}
+
+//Perform the game of life algorithm
+func worker(p Params, inputWorld [][]uint8) [][]uint8 {
+
+	//Create the result world
+	updatedWorld := make([][]uint8, p.ImageHeight)
+	for i := range updatedWorld {
+		updatedWorld[i] = make([]uint8, p.ImageWidth)
+	}
+
+	var edge int = p.ImageHeight - 1
+
+	//Go row by row
+	for i, row := range inputWorld {
+
+		//We find the rows above and below the current row the tile is at
+		var prevRow []uint8 = inputWorld[((i + len(updatedWorld) - 1) % len(updatedWorld))]
+		var nextRow []uint8 = inputWorld[((i + len(updatedWorld) + 1) % len(updatedWorld))]
+
+		//Go through the elements in each row
+		for j, tile := range row {
+
+			//populate a list of neighbors of the tile
+			var neighboursList []uint8 = populateNeighboursList(inputWorld, prevRow, row, nextRow, edge, j)
+
+			//Find number of adjacent tiles that are alive
+			var adjacentAliveCells int = 0
+			for _, value := range neighboursList {
 				if value > 0 {
-					num += 1
+					adjacentAliveCells++
 				}
 			}
-			// if the element is dead, then run through those checks
-			if element2 == 0 {
-				if num > 0 {
-					//fmt.Println(num, ", world", index2, index)
-				}
-				if num == 3 {
-					updatedWorld[index][index2] = 255
-				} else {
-					updatedWorld[index][index2] = 0
-				}
 
-			} else {
-				if num < 2 || num > 3 {
-					updatedWorld[index][index2] = 0
-				} else {
-					updatedWorld[index][index2] = 255
-				}
+			updatedWorld[i][j] = updateUpdatedWorldTile(tile, updatedWorld[i][j], adjacentAliveCells)
 
-			}
 		}
 	}
+
 	return updatedWorld
 }
 
@@ -108,6 +153,8 @@ func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
 
+	var turn int = 0
+
 	//We create the worlds
 	inputWorld := make([][]uint8, p.ImageHeight)
 	for i := 0; i < p.ImageHeight; i++ {
@@ -115,7 +162,7 @@ func distributor(p Params, c distributorChannels) {
 	}
 
 	// contact the io
-	c.ioFilename <- strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight)
+	c.ioFilename <- (strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight))
 	c.ioCommand <- 1
 
 	for i := 0; i < p.ImageHeight; i++ {
@@ -124,15 +171,11 @@ func distributor(p Params, c distributorChannels) {
 			inputWorld[i][j] = <-(c.ioInput)
 		}
 	}
-	turn := 0
 
-	fmt.Println(calculateAliveCells(p, inputWorld))
-
-	//updatedWorld = worker(p, inputWorld)
+	//Run the game of life algorithm for specified number of turns
 	for i := 0; i < p.Turns; i++ {
 		inputWorld = worker(p, inputWorld)
 		turn++
-		//fmt.Println("Passed turn")
 	}
 
 	// TODO: Execute all turns of the Game of Life.
