fafd0674179e762b19477ba6d3df1b10c0ea8a9f
ec22583 <ec22583@bristol.ac.uk>
2023-11-03 19:30:34 +0000

    changed all uint8 with byte type (synonym fr fr)

--- gol/distributor.go
+++ gol/distributor.go
@@ -18,12 +18,12 @@ type distributorChannels struct {
 	ioIdle    <-chan bool
 
 	ioFilename chan<- string
-	ioOutput   chan<- uint8
-	ioInput    <-chan uint8
+	ioOutput   chan<- byte
+	ioInput    <-chan byte
 }
 
 //Helper function to distributor to find the number of alive cells adjancent to the tile
-func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
 	coordinates := []util.Cell{}
 	for index, row := range world {
 		for index2, _ := range row {
@@ -35,7 +35,7 @@ func calculateAliveCells(p Params, world [][]uint8) []util.Cell {
 	return coordinates
 }
 
-func updateUpdatedWorldTile(inputWorldTile, updatedWorldTile uint8, adjacentAliveCells int) uint8 {
+func updateUpdatedWorldTile(inputWorldTile, updatedWorldTile byte, adjacentAliveCells int) byte {
 	// if the element is dead, then run through those checks
 	if inputWorldTile == DEAD {
 		if adjacentAliveCells > 0 {
@@ -59,8 +59,8 @@ func updateUpdatedWorldTile(inputWorldTile, updatedWorldTile uint8, adjacentAliv
 	return updatedWorldTile
 }
 
-func populateNeighboursList(inputWorld [][]uint8, prevRow, row, nextRow []uint8, edgeIndex, tileIndex int) []uint8 {
-	neighboursList := []uint8{
+func populateNeighboursList(inputWorld [][]byte, prevRow, row, nextRow []byte, edgeIndex, tileIndex int) []byte {
+	neighboursList := []byte{
 		prevRow[tileIndex],
 		nextRow[tileIndex]}
 
@@ -97,19 +97,19 @@ func populateNeighboursList(inputWorld [][]uint8, prevRow, row, nextRow []uint8,
 	return neighboursList
 }
 
-func manager(imageHeight int, imageWidth int, inputWorld [][]uint8, out chan<- [][]uint8, wg *sync.WaitGroup) {
+func manager(imageHeight int, imageWidth int, inputWorld [][]byte, out chan<- [][]byte, wg *sync.WaitGroup) {
 	gameSlice := worker(imageHeight, imageWidth, inputWorld)
 	out <- gameSlice
 	defer wg.Done()
 }
 
 //Perform the game of life algorithm
-func worker(imageHeight int, imageWidth int, inputWorld [][]uint8) [][]uint8 {
+func worker(imageHeight int, imageWidth int, inputWorld [][]byte) [][]byte {
 
 	//Create the result world
-	updatedWorld := make([][]uint8, imageHeight)
+	updatedWorld := make([][]byte, imageHeight)
 	for i := range updatedWorld {
-		updatedWorld[i] = make([]uint8, imageWidth)
+		updatedWorld[i] = make([]byte, imageWidth)
 	}
 
 	var edge int = imageHeight - 1
@@ -118,14 +118,14 @@ func worker(imageHeight int, imageWidth int, inputWorld [][]uint8) [][]uint8 {
 	for i, row := range inputWorld {
 
 		//We find the rows above and below the current row the tile is at
-		var prevRow []uint8 = inputWorld[((i + len(updatedWorld) - 1) % len(updatedWorld))]
-		var nextRow []uint8 = inputWorld[((i + len(updatedWorld) + 1) % len(updatedWorld))]
+		var prevRow []byte = inputWorld[((i + len(updatedWorld) - 1) % len(updatedWorld))]
+		var nextRow []byte = inputWorld[((i + len(updatedWorld) + 1) % len(updatedWorld))]
 
 		//Go through the elements in each row
 		for j, tile := range row {
 
 			//populate a list of neighbors of the tile
-			var neighboursList []uint8 = populateNeighboursList(inputWorld, prevRow, row, nextRow, edge, j)
+			var neighboursList []byte = populateNeighboursList(inputWorld, prevRow, row, nextRow, edge, j)
 
 			//Find number of adjacent tiles that are alive
 			var adjacentAliveCells int = 0
@@ -151,9 +151,9 @@ func distributor(p Params, c distributorChannels) {
 	var turn int = 0
 
 	//We create the worlds
-	inputWorld := make([][]uint8, p.ImageHeight)
+	inputWorld := make([][]byte, p.ImageHeight)
 	for i := 0; i < p.ImageHeight; i++ {
-		inputWorld[i] = make([]uint8, p.ImageWidth)
+		inputWorld[i] = make([]byte, p.ImageWidth)
 	}
 
 	// contact the io
@@ -178,16 +178,16 @@ func distributor(p Params, c distributorChannels) {
 
 	//Run the game of life algorithm for specified number of turns
 	for i := 0; i < p.Turns; i++ {
-		var newWorld [][]uint8
+		var newWorld [][]byte
 		//If there's only one worker, run as normal without the manager
 		if p.Threads == 1 {
 			newWorld = worker(p.ImageHeight, p.ImageWidth, inputWorld)
 		} else {
 			//If there's more than one worker, set up a wait group and comms channels for each strip
 			var wg sync.WaitGroup
-			var genSlice []chan [][]uint8
+			var genSlice []chan [][]byte
 			for j := 0; j < p.Threads; j++ {
-				newSlice := make(chan [][]uint8, 2)
+				newSlice := make(chan [][]byte, 2)
 				genSlice = append(genSlice, newSlice)
 			}
 			//For each thread, split the input world and pass it to the manager goroutine
@@ -196,25 +196,26 @@ func distributor(p Params, c distributorChannels) {
 				var startIndex int
 				var midIndex int
 				var endIndex int
-				var strip [][]uint8
+				var strip [][]byte
 				//If it's the first thread, it should have the last line at [0]
 				if j == 0 {
 					startIndex = p.ImageHeight - 1
 					midIndex = 0
 					endIndex = (j + 1) * int(StripSize)
-					strip = append([][]uint8{inputWorld[startIndex]}, inputWorld[midIndex:endIndex]...)
+					strip = append([][]byte{inputWorld[startIndex]}, inputWorld[midIndex:endIndex]...)
 				} else {
 					startIndex = j*int(StripSize) - 1
 					midIndex = j * int(StripSize)
 					//If it's the final thread, it should have the first line at [StripSize-1]
 					if j == p.Threads-1 {
-						//fmt.Println("hi")
 						endIndex = 0
-						strip = append([][]uint8{inputWorld[startIndex]}, inputWorld[midIndex:p.ImageHeight]...)
+						strip = append([][]byte{inputWorld[startIndex]}, inputWorld[midIndex:p.ImageHeight]...)
 						StripSize = float64(p.ImageHeight - startIndex - 1)
 					} else {
+
+						//Bruh this is the section that fails
 						endIndex = (j+1)*int(StripSize) + 1
-						strip = append([][]uint8{inputWorld[startIndex]}, inputWorld[midIndex:endIndex-1]...)
+						strip = append([][]byte{inputWorld[startIndex]}, inputWorld[midIndex:endIndex-1]...)
 						if endIndex == p.ImageHeight-1 {
 							endIndex = 0
 						}
