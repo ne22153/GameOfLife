33870b151d0bf53b1ee718a9a79773c4d600dfc5
ec22583 <ec22583@bristol.ac.uk>
2023-11-07 17:43:24 +0000

    refactor game of life alg in distributor section

--- Distributed/gameOfLife.go
+++ Distributed/gameOfLife.go
@@ -29,9 +29,25 @@ func updateUpdatedWorldTile(inputWorldTile, updatedWorldTile byte, adjacentAlive
 	return updatedWorldTile
 }
 
+//We add up the values of all neighbouring cells and then divide it by LIVE to determine
+//living neighbours count
+func calculateAdjacentAlive(inputWorld [][]byte, i, j, imageHeight, imageWidth int) int {
+	adjacentAliveCells :=
+		int(inputWorld[(i-1+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
+			int(inputWorld[(i-1+imageHeight)%imageHeight][(j+imageWidth)%imageWidth]) +
+			int(inputWorld[(i-1+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth]) +
+			int(inputWorld[(i+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
+			int(inputWorld[(i+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth]) +
+			int(inputWorld[(i+1+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
+			int(inputWorld[(i+1+imageHeight)%imageHeight][(j+imageWidth)%imageWidth]) +
+			int(inputWorld[(i+1+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth])
+	adjacentAliveCells = adjacentAliveCells / LIVE
+
+	return adjacentAliveCells
+}
+
 //Perform the game of life algorithm
 func worker(imageHeight int, imageWidth int, inputWorld [][]byte) [][]byte {
-
 	//Create the result world
 	updatedWorld := make([][]byte, imageHeight)
 	for i := range updatedWorld {
@@ -40,30 +56,11 @@ func worker(imageHeight int, imageWidth int, inputWorld [][]byte) [][]byte {
 
 	//Go row by row
 	for i, row := range inputWorld {
-
-		//We find the rows above and below the current row the tile is at
-
-		//Go through the elements in each row
 		for j, tile := range row {
-
 			adjacentAliveCells := 0
-
-			//We add up the values of all neighbouring cells and then divide it by LIVE to determine
-			//living neighbours count
-			adjacentAliveCells =
-				int(inputWorld[(i-1+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
-					int(inputWorld[(i-1+imageHeight)%imageHeight][(j+imageWidth)%imageWidth]) +
-					int(inputWorld[(i-1+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth]) +
-					int(inputWorld[(i+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
-					int(inputWorld[(i+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth]) +
-					int(inputWorld[(i+1+imageHeight)%imageHeight][(j-1+imageWidth)%imageWidth]) +
-					int(inputWorld[(i+1+imageHeight)%imageHeight][(j+imageWidth)%imageWidth]) +
-					int(inputWorld[(i+1+imageHeight)%imageHeight][(j+1+imageWidth)%imageWidth])
-			adjacentAliveCells = adjacentAliveCells / LIVE
-
+			adjacentAliveCells = calculateAdjacentAlive(inputWorld, i, j, imageHeight, imageWidth)
 			updatedWorld[i][j] = updateUpdatedWorldTile(tile, updatedWorld[i][j], adjacentAliveCells)
 		}
 	}
-
 	return updatedWorld
 }
