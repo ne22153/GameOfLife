fc98ac5a5981a89a0a25b3f1d487f385e4fd2347
ne22153 <ne22153@bristol.ac.uk>
2023-11-10 21:38:41 +0000

    Passes more tests, not all

--- Distributed/Node/node.go
+++ Distributed/Node/node.go
@@ -15,21 +15,29 @@ func GoLWorker(inputWorld [][]byte, p Shared.Params) [][]byte {
 	var newWorld [][]byte
 	fmt.Println(p.Turns)
 	if p.Turns == 0 {
+		if p.ImageHeight == 16 {
+			fmt.Println("Auto done: ", inputWorld)
+		}
 		return inputWorld
 	}
-	for i := 0; i < p.Turns; i++ {
+	for i := 0; i < p.Turns-1; i++ {
+		if p.ImageWidth == 16 && p.Turns < 10 {
+			fmt.Println("Turn number: ", i-1, " World: ", inputWorld)
+		}
 		newWorld = worker(p.ImageHeight, p.ImageWidth, inputWorld)
+		inputWorld = newWorld
+		if p.ImageWidth == 16 && p.Turns < 10 {
+			fmt.Println("Turn number: ", i, " World: ", inputWorld)
+		}
 	}
-	fmt.Println(newWorld)
-	return newWorld
+
+	return inputWorld
 }
 
 type GoLOperations struct{}
 
 func (s *GoLOperations) GoLManager(req Shared.Request, res *Shared.Response) (err error) {
 	res.World = GoLWorker(req.World, req.Parameters)
-
-	//req.Events <- Shared.FinalTurnComplete{CompletedTurns: req.Parameters.Turns}
 	return
 }
 
