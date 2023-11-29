f7471e4adc56190904eee76bc87c0860a4bea108
ec22583 <ec22583@bristol.ac.uk>
2023-11-29 16:39:26 +0000

    Updated the benchmark file

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,6 +7,8 @@ import (
 	"uk.ac.bris.cs/gameoflife/gol"
 )
 
+// From median filter in concurrency lab 1
+//Modified for the purposes of showing 1-16 threads instead of powers of 2
 // Benchmark applies the filter to the ship.png b.N times.
 // The time taken is carefully measured by go.
 // The b.N  repetition is needed because benchmark results are not always constant.
@@ -14,8 +16,8 @@ func BenchmarkFilter(b *testing.B) {
 	// Disable all program output apart from benchmark results
 	os.Stdout = nil
 
-	// Use a for-loop to run 5 sub-benchmarks, with 1, 2, 4, 8 and 16 workers.
-	for threads := 1; threads <= 16; threads *= 2 {
+	// Use a for-loop to run
+	for threads := 1; threads <= 16; threads++ {
 		b.Run(fmt.Sprintf("%d_workers", threads), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 
